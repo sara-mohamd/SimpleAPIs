74389f8725d8a8616627acb2ebc336dba21b6674
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-20 15:19:04 +0300

    methods update

--- 0x02-python-object_relational_mapping/mysql-SQLAlchemy.py
+++ 0x02-python-object_relational_mapping/mysql-SQLAlchemy.py
@@ -6,24 +6,64 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1130@localhost/Emp
 
 db = SQLAlchemy(app)
 class Employee(db.Model):
-  """
-  Creating one table using ORM
-  """
+    """
+    Creating one table using ORM
+    """
 
-  __tablename__ = 'Employee'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(255), nullable=False)
-  gender = db.Column(db.String(10), nullable=False)
-  salary = db.Column(db.Numeric(10, 2), nullable=False)
+    __tablename__ = 'Employee'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    salary = db.Column(db.Numeric(10, 2), nullable=False)
 
-  def __str__(self):
-    return f"name: {self.name}\ngender: {self.gender}\nsalary: {self.salary}"
+
+    # Constructor
+    def __init__(self, name='', gender='', salary=''):
+          self.name = name
+          self.salary = salary
+          self.gender = gender
+
+
+    def to_dict(self):
+        return {
+              'id': self.id,
+              'name': self.name,
+              'gender': self.gender,
+              'salary': str(self.salary)
+          }
 
 @app.route('/employee', methods=['GET'])
 def get_employees():
-  emp = Employee.query.all()
-  return jsonify(emp)
+    emp = Employee.query.all()
+    return jsonify([e.to_dict() for e in emp])
 
+@app.route('/employee/<int:id>', methods=['GET'])
+def get_employee(id):
+    emp = Employee.query.get(id)
+    if emp is None:
+        abort(404)
+    
+    return jsonify(emp.to_dict())
+
+@app.route('/employee', methods=['POST'])
+def insert_emp():
+    if not request.json or not all(k in request.json for k in ('name', 'gender', 'salary')):
+        abort(400) # refer to bad request
+
+    emp_info = Employee(request.json['name'],request.json['gender'], request.json['salary'])
+    db.session.add(emp_info)
+    db.session.commit()
+
+    return jsonify("Employer inserted!")
+
+
+@app.route('/employee/<int:id>', methods=['PUT'])
+def update_emp():
+    ...
+
+@app.route('/employee/<int:id>', methods=['DELETE'])
+def delete_emp(id):
+    ...
 
 if __name__ == '__main__':
   app.run(debug=True)
