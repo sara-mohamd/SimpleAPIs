80c49379592578a967436b9b0e80cc3cb81d9d77
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-21 09:18:48 +0300

    CRUD operations to student

--- student-courses-system/apps/routes/student.py
+++ student-courses-system/apps/routes/student.py
@@ -12,16 +12,38 @@ def get_students():
 
 @studentBP.route('/<int:id>',methods=['GET'])
 def get_student(id):
-    ...
+    emp = Student.query.get(id)
+    if emp is None:
+        abort(404)
+
+    return jsonify(emp.to_dict())
 
 @studentBP.route('/', methods=['POST'])
 def insert_student():
-    if not request.json or not all(x in request.json for x in ('name', 'email', 'enrolled_data')):
+    if not request.json or not all(x in request.json for x in ('name', 'email')):
         abort(400)
     stu_info = Student(request.json['name'], request.json['email'])
     db.session.add(stu_info)
-    db.commit()
-
+    db.session.commit()
     return jsonify("New Studenr Enrolled!")
 
+@studentBP.route('/<int:id>', methods=['PUT'])
+def update_email(id):
+    stu = Student.query.get(id)
+    if stu is None:
+        abort(404, description="email is empty")
+    if not request.json or not request.json['email']:
+        abort(400, description='There is no either request message or email included!')
+    stu.email = request.json['email']
+    db.session.commit()
+    return jsonify(stu.to_dict())
 
+
+@studentBP.route('/<int:id>', methods=['DELETE'])
+def delete_stu(id):
+    stu = Student.query.get(id)
+    if stu is None:
+        abort(404, description='Student not found')
+    db.session.delete(stu)
+    db.session.commit()
+    return jsonify('Student was removed')
